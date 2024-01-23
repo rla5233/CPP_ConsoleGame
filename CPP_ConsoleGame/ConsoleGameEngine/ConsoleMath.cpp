14c8cb016067864f08f6b21c8eaae1f3c6809da5
rla5233 <rla5233@naver.com>
2024-01-23 22:23:26 +0900

    ConsoleMath 업데이트

---
+++ CPP_ConsoleGame/ConsoleGameEngine/ConsoleMath.cpp
@@ -0,0 +1,7 @@
+#include "ConsoleMath.h"
+
+const Point Point::Left		= { -1,  0 };
+const Point Point::Right	= {  1,  0 };
+const Point Point::Up		= {  0, -1 };
+const Point Point::Down		= {  0,  1 };
+const Point Point::Zero		= {  0,  0 };
---
+++ CPP_ConsoleGame/ConsoleGameEngine/ConsoleMath.h
@@ -0,0 +1,34 @@
+#pragma once
+
+// 설명 : 좌표 클래스
+class Point
+{
+public:
+	static const Point Left;
+	static const Point Right;
+	static const Point Up;
+	static const Point Down;
+	static const Point Zero;
+
+public:
+	int X = 0;
+	int Y = 0;
+
+	void operator=(const Point& _Other)
+	{
+		X = _Other.X;
+		Y = _Other.Y;
+	}
+
+	bool operator==(const Point& _Other)
+	{
+		return X == _Other.X && Y == _Other.Y;
+	}
+	
+	bool operator!=(const Point& _Other)
+	{
+		return X != _Other.X || Y == _Other.Y;
+	}
+	
+};
+
