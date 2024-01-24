57d92df4534b080829617327dd1c5678ab6a4587
rla5233 <rla5233@naver.com>
2024-01-25 00:47:02 +0900

    업데이트

--- CPP_ConsoleGame/ConsoleGameEngine/EngineCore.cpp
+++ CPP_ConsoleGame/ConsoleGameEngine/EngineCore.cpp
@@ -25,9 +25,35 @@ void EngineCore::EngineStart()
 		Sleep(100);
 
 		// 매니저 업데이트
+		{
+			for (std::pair<const int, ConsoleUpdater*>& Order : AllManager)
+			{
+				ConsoleUpdater* Object = Order.second;
+				if (nullptr == Object)
+				{
+					MsgBoxAssert("Manager Nullptr Error");
+				}
 
+				Object->Update();
+			}
+		}
 
 		// 오브젝트 업데이트
+		{
+			for (std::pair<const int, std::list<ConsoleObject*>>& Order : AllUpdateObject)
+			{
+				std::list<ConsoleObject*>& ObjectList = Order.second;
+				for (ConsoleObject* Object : ObjectList)
+				{
+					if (nullptr == Object)
+					{
+						MsgBoxAssert("Update Object Nullptr Error");
+					}
+					
+					Object->Update();
+				}
+			}
+		}
 
 
 		// 렌더링
