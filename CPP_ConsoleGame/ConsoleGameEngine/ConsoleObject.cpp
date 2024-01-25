513768e5157dc3a449832823b1b9d3625df51549
rla5233 <rla5233@naver.com>
2024-01-25 16:46:10 +0900

    ConsoloObject : Collision 추가

--- CPP_ConsoleGame/ConsoleGameEngine/ConsoleObject.cpp
+++ CPP_ConsoleGame/ConsoleGameEngine/ConsoleObject.cpp
@@ -16,7 +16,24 @@ ConsoleObject::~ConsoleObject()
 
 ConsoleObject* ConsoleObject::Collision(int _UpdateOrder)
 {
-	//std::list<ConsoleObject*>& ObjectList = ;
+	std::list<ConsoleObject*>& ObjectList = GetCore()->AllUpdateObject[_UpdateOrder];
+	for (ConsoleObject* Object : ObjectList)
+	{
+		if (nullptr == Object)
+		{
+			MsgBoxAssert("Collision Error : Object is Nullptr");
+		}
+
+		if (this == Object)
+		{
+			continue;
+		}
+
+		if (GetPos() == Object->GetPos())
+		{
+			return Object;
+		}
+	}
 
 	return nullptr;
 }
